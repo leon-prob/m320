e7a60cb1aed1e85d9ec6a382291569702e7f5a67
leon-prob <leon.probst@edu.tbz.ch>
2025-09-02 08:20:10 +0200

    Add banking application classes and diagram

--- Repetition/src/ch/tbz/leon_michel/Main.java
+++
@@ -1,9 +0,0 @@
-package ch.tbz.leon_michel;
-
-import ch.tbz.leon_michel.guess_number.Game;
-
-public class Main {
-    public static void main(String[] args) {
-        Game game = new Game(0,10);
-    }
-}
